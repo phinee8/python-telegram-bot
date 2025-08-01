44caaf03ad8337aa39644529f3b427028c66baa8
phinee8 <phineekapenda@gmail.com>
2025-08-01 13:06:51 +0200

    Update __main__.py
    Bot Telegram pour démarrer et aider les utilisateurs.

--- src/telegram/__main__.py
+++ src/telegram/__main__.py
@@ -1,4 +1,28 @@
-# !/usr/bin/env python
+```python
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+
+TOKEN = "6251031144:AAFG7VEvwlONa89Xp-nFjInIUWmP8PXBIQE"
+
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text("Bonjour! Je suis ton bot. Comment puis-je t'aider?")
+
+def help_command(update: Update, context: CallbackContext):
+    update.message.reply_text("Voici les commandes disponibles:\n/start - démarrer le bot\n/help - aide")
+
+def main():
+    updater = Updater(TOKEN)
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+
+    updater.start_polling()
+    updater.idle()
+
+if _name_ == "_main_":
+    main()
+```# !/usr/bin/env python
 #
 # A library that provides a Python interface to the Telegram Bot API
 # Copyright (C) 2015-2025
